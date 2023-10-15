c0f87a71921c3c9b9c41cb99dcaf370a1d31a28c
Harun Kizilkaya <h.kizilkaya@web.de>
2023-10-15 14:21:43 +0200

    Initial Commit

---
+++ OwnToDoPractice/OwnToDoPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnToDoPracticeApp.swift
+//  OwnToDoPractice
+//
+//  Created by Harun Kizilkaya on 15.10.23.
+//
+
+import SwiftUI
+
+@main
+struct OwnToDoPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
